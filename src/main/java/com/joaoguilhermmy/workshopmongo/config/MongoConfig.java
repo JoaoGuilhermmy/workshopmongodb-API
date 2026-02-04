bc91540644b5f142f061d889679d2ec6a255506f
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-02-04 13:30:52 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/joaoguilhermmy/workshopmongo/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package com.joaoguilhermmy.workshopmongo.config;
+
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory() {
+        return new SimpleMongoClientDatabaseFactory(
+                MongoClients.create("mongodb://localhost:27017"),
+                "workshop_mongo");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDatabaseFactory());
+    }
+}
