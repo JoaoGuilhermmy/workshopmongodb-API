ce656e40dc111368b8f25ae3b1c1baf9746affcb
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-02-05 09:17:54 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/joaoguilhermmy/workshopmongo/config/Instantiation.java
+++ src/main/java/com/joaoguilhermmy/workshopmongo/config/Instantiation.java
@@ -1,16 +1,22 @@
 package com.joaoguilhermmy.workshopmongo.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.joaoguilhermmy.workshopmongo.domain.Post;
 import com.joaoguilhermmy.workshopmongo.domain.User;
+import com.joaoguilhermmy.workshopmongo.repository.PostRepository;
 import com.joaoguilhermmy.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -19,12 +25,18 @@ public class Instantiation implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, Instant.now(), "Partiu Viagem", "Vou viajar para São Paulo, abraços!", maria);
+        Post post2 = new Post(null, Instant.now(), "Bom dia", "Acordei feliz hoje!", maria);
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
     }
 
 }
