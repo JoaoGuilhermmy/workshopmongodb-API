ac6e245448b7cfba2e546d70e48f80b242e624ff
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-02-05 00:19:11 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/joaoguilhermmy/workshopmongo/service/UserService.java
+++ src/main/java/com/joaoguilhermmy/workshopmongo/service/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.joaoguilhermmy.workshopmongo.domain.User;
 import com.joaoguilhermmy.workshopmongo.dto.UserDTO;
+import com.joaoguilhermmy.workshopmongo.expection.DatabaseException;
 import com.joaoguilhermmy.workshopmongo.expection.ResourcesNotFoundException;
 import com.joaoguilhermmy.workshopmongo.repository.UserRepository;
 
@@ -29,6 +31,18 @@ public class UserService {
 
     public User insert(User obj) {
         return repository.save(obj);
+    }
+
+    public void delete(String id) {
+
+        if (!repository.existsById(id)) {
+            throw new ResourcesNotFoundException(id);
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User fromDTO(UserDTO objDto) {
