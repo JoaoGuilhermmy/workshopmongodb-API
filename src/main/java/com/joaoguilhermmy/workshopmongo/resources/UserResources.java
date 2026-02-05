eb9b5a2ab6c7b44d0323a111ff89e94c29d3b66d
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-02-05 09:47:48 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/joaoguilhermmy/workshopmongo/resources/UserResources.java
+++ src/main/java/com/joaoguilhermmy/workshopmongo/resources/UserResources.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.joaoguilhermmy.workshopmongo.domain.Post;
 import com.joaoguilhermmy.workshopmongo.domain.User;
 import com.joaoguilhermmy.workshopmongo.dto.UserDTO;
 import com.joaoguilhermmy.workshopmongo.service.UserService;
@@ -61,6 +62,12 @@ public class UserResources {
         user.setId(id);
         user = service.update(user);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 
 }
